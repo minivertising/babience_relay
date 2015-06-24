c627a40d714aa20b18e0ac9a9fddd05ec28ff631
kyhfan <yh.kim@minivertising.kr>
2015-06-24 19:19:31 +0900

    1111

--- include/function.php
+++ include/function.php
@@ -228,12 +228,8 @@ $mydir = "testfolder";
  if(@mkdir($mydir, 0777)) { 
     if(is_dir($mydir)) { 
         @chmod($mydir, 0777); 
-        echo "${mydir} 디렉토리를 생성하였습니다."; 
     } 
- } 
- else { 
-    echo "${mydir} 디렉토리를 생성하지 못했습니다."; 
- } 
+ }
     switch ($arrImgInfo[2])
     {
 		case 1:
