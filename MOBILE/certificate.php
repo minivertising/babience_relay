6638cbadcb6198676402a8231d7293d4b1339a65
kyhfan <yh.kim@minivertising.kr>
2015-06-26 21:24:08 +0900

    모바일 기부증서 페이지

--- MOBILE/certificate.php
+++ MOBILE/certificate.php
@@ -7,9 +7,6 @@
 	$query	= "SELECT * FROM ".$_gl['member_info_table']." WHERE mb_serialnumber='".$serial."'";
 	$result 	= mysqli_query($my_db, $query);
 	$member_info	= mysqli_fetch_array($result);
-
-	//print_r($member_info);
-	echo phpinfo();
 ?>
 <!doctype html>
 <html>
@@ -54,25 +51,83 @@
 
   </head>
 
-  <body>
-    <div>
-	  <img src="<?=$member_info['mb_image']?>">
-	</div>
-	<div>
-	  <a href="#" onclick="sns_share('fb');">페이스북</a>
-	  <a href="#" onclick="sns_share('ks');">카스</a>
-	  <a href="#" onclick="sns_share('kt');">카톡</a>
-	  <a href="#" onclick="sns_share('tw');">트위터</a>
-	</div>
-	<div>
-	  <a href="#">선물번호 확인하기</a>
-	</div>
-  </body>
+<div class="sec_give_paper_block">
+  <div class="name"><?=$member_info['baby_name']?></div>
+  <div class="btn">
+    <a href="#"><img src="images/lms_sns_kt.png" alt=""/></a>
+    <a href="#"><img src="images/lms_sns_ks.png" alt=""/></a>
+    <a href="#"><img src="images/lms_sns_fb.png" alt=""/></a>
+  </div>
+  <div class="bg"><img src="images/lms_bg.jpg" alt=""/></div>
+</div>
+
+<div class="sec_lms_gift">
+  <div class="title">
+    <img src="images/lms_gift_title.jpg" />
+  </div>
+  <div class="list_block">
+    <div class="list_one label clearfix">
+      <div class="gift_name">선물</div>
+      <div class="gift_num">선물 번호</div>
+    </div>
+  </div> 
+  <div class="list_block">
+<?
+	$query 	= "SELECT * FROM ".$_gl['member_info_table']." WHERE mb_phone='".$member_info['mb_phone']."'";
+	$result 	= mysqli_query($my_db, $query);
+
+	$i = 0;
+	$style_css	= "";
+	while ($data = mysqli_fetch_array($result))
+	{
+		if ($i > 5)
+			$style_css	= "style='display:none;'";
+
+		if ($data['mb_winner'] == "CASH")
+		{
+			$winner_gift	="베비언스 3천원 쿠폰";
+		}else if ($data['mb_winner'] == "CAMERA"){
+			$winner_gift	="DSLR 카메라";
+		}else if ($data['mb_winner'] == "HOTEL"){
+			$winner_gift	="하얏트 호텔 숙박권";
+		}else if ($data['mb_winner'] == "WG"){
+			$winner_gift	="베베프람 웨건";
+		}else if ($data['mb_winner'] == "MILK"){
+			$winner_gift	="베비언스 분유 1년치";
+		}else if ($data['mb_winner'] == "WATER"){
+			$winner_gift	="베이비워터 24병";
+		}else if ($data['mb_winner'] == "WASH"){
+			$winner_gift	="메소드 핸드워시";
+		}
+?>
+    <div class="list_one clearfix" <?=$style_css?>>
+      <div class="gift_name"><?=$winner_gift?></div>
+      <div class="gift_num"><?=$data['mb_serialnumber']?></div>
+      <div class="btn"><a href="#" onclick="copy_url('<?=$data['mb_serialnumber']?>')"><img src="images/btn_copy.png" alt=""/></a></div>
+    </div>
+<?
+		$i++;
+	}
+?>
+    <div class="btn_more">
+      <a href="#" onclick="more_show();return false;">더보기</a>
+    </div>
+  </div>         
+</div>  
+
+<div class="sec_lms_gift_btn">
+  <a href="#" target="_blank"><img src="images/lms_gift_btn.jpg" /></a>
+</div>
+
+<div class="sec_lms_footer">
+  <img src="images/lms_footer.jpg" />
+</div>
+</body>
 </html>
 <script type="text/javascript">
 Kakao.init('b9c52d3d573fd09cbe25e306fafc5df6');
 
-function sns_share(media)
+function c_sns_share(media)
 {
 	if (media == "fb")
 	{
@@ -169,4 +224,24 @@ function sns_share(media)
 	}
 }
 
+function copy_url(ss_url)
+{
+	//window.clipboardData.setData('text',"11<?=$_SESSION['ss_url']?>");
+    //alert("클립보드에 복사되었습니다.");
+	//var text = $("#serial_number").val();
+	var text = ss_url;
+	if(window.clipboardData){
+		// IE처리
+		// 클립보드에 문자열 복사
+		window.clipboardData.setData('text', text);
+	} else {
+		// 비IE 처리    
+		window.prompt ("선물번호를 복사해주세요!", text);  
+	}
+}
+
+function more_show()
+{
+	$(".list_one").css("display","block");
+}
 </script>
