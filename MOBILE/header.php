923c258cb4ce72319c121e5b6c9384e4d8f2ea11
kyhfan <yh.kim@minivertising.kr>
2015-07-01 14:10:53 +0900

    페이스북 전환 픽셀 삽입

--- MOBILE/header.php
+++ MOBILE/header.php
@@ -43,4 +43,20 @@
   ga('send', 'pageview');
 
 </script>
+<!-- Facebook Conversion Code for 베비언스_잠재 고객 - lgcare&#064;incross.com 1 -->
+<script>(function() {
+var _fbq = window._fbq || (window._fbq = []);
+if (!_fbq.loaded) {
+var fbds = document.createElement('script');
+fbds.async = true;
+fbds.src = '//connect.facebook.net/en_US/fbds.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(fbds, s);
+_fbq.loaded = true;
+}
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6028282329497', {'value':'0.00','currency':'USD'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6028282329497&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
   </head>
