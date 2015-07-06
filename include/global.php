1349602323a5c7e5247e4b5a35278391178afa6d
kyhfan <yh.kim@minivertising.kr>
2015-07-06 10:44:58 +0900

    22222

--- include/global.php
+++ include/global.php
@@ -28,26 +28,6 @@
 	$_gl['prize']['cash']					= "CASH";		// 베이언스 3천원 쿠폰
 
 
-			if ($search_data['mb_winner'] == "CASH")
-			{
-				$winner_gift	="베비언스 3천원 쿠폰";
-			}else if ($search_data['mb_winner'] == "CAMERA"){
-				$winner_gift	="DSLR 카메라";
-			}else if ($search_data['mb_winner'] == "HOTEL"){
-				$winner_gift	="하얏트 호텔 숙박권";
-			}else if ($search_data['mb_winner'] == "WG"){
-				$winner_gift	="베베프람 웨건";
-			}else if ($search_data['mb_winner'] == "MILK"){
-				$winner_gift	="베비언스 분유 1년치";
-			}else if ($search_data['mb_winner'] == "WATER"){
-				$winner_gift	="베이비워터 24병";
-			}else if ($search_data['mb_winner'] == "WASH"){
-				$winner_gift	="메소드 핸드워시";
-			}
-
-
-
-
 	$_gl['week']['1']					= "1";		// 1주차
 	$_gl['week']['2']					= "2";		// 2주차
 	$_gl['week']['3']					= "3";		// 3주차
